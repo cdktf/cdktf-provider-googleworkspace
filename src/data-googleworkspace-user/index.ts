275ad48e58209e554c163cfb509d321db6d95234
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-09-18 15:07:04 +0000

    chore(deps): Updated  by cdktf/cdktf-repository-manager@d2a7f81

--- src/data-googleworkspace-user/index.ts
+++ src/data-googleworkspace-user/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/googleworkspace/0.7.0/docs/data-sources/user
 // generated from terraform resource schema
 
